134b671277a5bbbeebf694f94a4d974d6e95b8b4
briantant3 <235910952@qq.com>
2025-06-30 12:30:53 +0800

    Create generate-ipcidr.py

---
+++ generate-ipcidr.py
@@ -0,0 +1,26 @@
+import socket
+import ipaddress
+
+hostnames = [
+    "www.ecobee.com",
+    "api.ecobee.com",
+    "home-fw.hm-prod.ecobee.com",
+    "ls-api.ecobee.com",
+]
+
+rules = []
+for host in hostnames:
+    try:
+        ips = socket.gethostbyname_ex(host)[2]
+        for ip in ips:
+            obj = ipaddress.ip_address(ip)
+            if obj.version == 4:
+                rules.append(f"IP-CIDR,{ip}/32,EcobeeProxy")
+            else:
+                rules.append(f"IP-CIDR6,[{ip}]/128,EcobeeProxy")
+    except Exception as e:
+        rules.append(f"# {host} lookup failed: {e}")
+
+rules = sorted(set(rules))
+with open("ecobee-ipcidr.txt", "w") as f:
+    f.write("\n".join(rules))
